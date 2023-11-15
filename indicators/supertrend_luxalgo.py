a0026b9521102b51685221e8131339642f1d77e5
Vahid Moradi <vahid.moradi001@gmail.com>
2023-11-15 06:47:20 +0000

    Removed initial_data from git

--- indicators/supertrend_luxalgo.py
+++ indicators/supertrend_luxalgo.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import talib
+
+
 class Supertrend:
     def __init__(self, upper, lower, output, perf, factor, trend):
         self.upper = upper
@@ -13,3 +17,20 @@ holder = []
 
 # List of factors
 factors = []
+
+
+def supertrend_ai(
+    length=111,
+    minMult=1,
+    maxMult=5,
+    step=0.5,
+    perfAlpha=10,
+    fromCluster="Best",
+    maxIter=1000,
+    maxData=10000,
+):
+    # Populate Supertrend type array
+    for i in range(int((maxMult - minMult) / step) + 1):
+        factor_value = minMult + i * step
+        factors.append(factor_value)
+        holder.append(Supertrend(0, 0, 0, 0, factor_value, 0))
